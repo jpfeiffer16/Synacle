da803f18c8bd262ead2ed5d52b4d325fced61cba
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-03-17 13:58:58 -0600

    Interpreter is complete in theory

--- interprreterr.js
+++ interprreterr.js
@@ -8,9 +8,59 @@ const machineInstructions = {
   1: {
     instruction: 'set',
     action: (memory) => {
-      const a = getValue();
-      const b = getValue();
+      const a = getValueAtInPtr();
+      memory.inPtr++;
+      const b = getValueAtInPtr();
+      memory.inPtr++;
       memory.registers[a] = b;
+    }
+  },
+  6: {
+    instruction: 'jmp',
+    action: (memory) => {
+      const a = getValueAtInPtr();
+      memory.inPtr++;
+      const actualA = a % 32768;
+      stackPtr = a - 1;
+    }
+  },
+  7: {
+    instruction: 'jt',
+    action: (memory) => {
+      const a = getValueAtInPtr();
+      memory.inPtr++;
+      const actualA = a % 32768;
+      const b = heap[++stackPtr];
+      if (actualA !== 0) {
+        stackPtr = b - 1;
+      }
+    }
+  },
+  8: {
+    instruction: 'jf',
+    action: (memory) => {
+      const a = getValueAtInPtr();
+      memory.inPtr++;
+      const actualA = a % 32768;
+      const b = getValueAtInPtr();
+      memory.inPtr++;
+      if (actualA === 0) {
+        stackPtr = b - 1;
+      }
+    }
+  },
+  19: {
+    instruction: 'out',
+    action: (memory) => {
+      const a = getValueAtInPtr();
+      const char = String.fromCharCode(a);
+      process.stdout.write(char);
+    }
+  },
+  21: {
+    instruction: 'noop',
+    action: function() {
+      //NOOP
     }
   }
 }
@@ -30,7 +80,7 @@ function getValueAtInPtr(memory) {
     return addressInt
   }
   if (addressInt >= 32768 && addressInt <= 32775) {
-    return registers[addressInt - 32768];
+    return memory.registers[addressInt - 32768];
   }
   return 0;
 }
