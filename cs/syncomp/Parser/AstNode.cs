e8e8f33e05dacb46c17c63c8dfa495a8ec4ae7b5
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-09-03 22:53:41 -0600

    Some cleanup

--- cs/syncomp/Parser/AstNode.cs
+++ cs/syncomp/Parser/AstNode.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 namespace syncomp {
     public class AstNode {
         public string Type { get; private set;}
-        // public List<AstNode> Nodes { get; set; }
 
         public AstNode(string type) {
             this.Type = type;
