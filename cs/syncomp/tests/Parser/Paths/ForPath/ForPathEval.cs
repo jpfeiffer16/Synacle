ae1ee3ff348eb703c07a2d1ca12e73c22c5aee51
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-10-15 23:06:37 -0600

    More tests

--- cs/syncomp/tests/Parser/Paths/ForPath/ForPathEval.cs
+++ cs/syncomp/tests/Parser/Paths/ForPath/ForPathEval.cs
@@ -6,6 +6,9 @@ namespace syncomp.Tests
   [TestClass]
   public class ForPathEval : Behavior
   {
+    private (int, AstNode) result;
+    private For forNode;
+
     protected override void Given()
     {
       var tokens = new List<SyntaxToken>
@@ -13,8 +16,97 @@ namespace syncomp.Tests
         new SyntaxToken
         {
           Type = SyntaxTokenType.For,
+          Token = "for"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.LeftParen,
+          Token = "("
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.VariableDeclaration,
+          Token = "var"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.Identifier,
+          Token = "i"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.VariableAssignment,
+          Token = "="
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.Identifier,
+          Token = "0"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.SemiColon,
+          Token = ";"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.Identifier,
+          Token = "i"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.LessThan,
+          Token = "<"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.Identifier,
+          Token = "10"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.SemiColon,
+          Token = ";"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.Incr,
+          Token = "++"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.Identifier,
+          Token = "i"
+        },
+        new SyntaxToken
+        {
+          Type = SyntaxTokenType.RightParen,
+          Token = ")"
         }
-      }
+      };
+      var nodes = new List<AstNode>();
+      var index = 0;
+
+      this.result = new ForPath().Eval(index, tokens, nodes);
+      this.forNode = (For)this.result.Item2;
+    }
+
+    [TestMethod]
+    public void IndexIsCorrect()
+    {
+      Assert.AreEqual(10, this.result.Item1);
+    }
+  
+    [TestMethod]
+    public void AstNodeIsCorrect()
+    {
+      Assert.IsInstanceOfType(this.result.Item2, typeof(For));
+    }
+
+    [TestMethod]
+    public void AstNodeInitIsCorrect()
+    {
+      Assert.IsInstanceOfType(this.forNode.Init, typeof(VariableAssignment));
     }
   }
 }
