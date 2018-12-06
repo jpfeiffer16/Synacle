8c2de41f9b7ea2b660cc358d71148a97c5728d54
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-12-05 20:34:03 -0700

    Revert "Scaffold because typing is boring, and I'm bad at it."
    This reverts commit b0bbcbe27e6e57b5589d84ab6541390b787086af.

--- cs/syncomp/tests/Transformer/Emitters/AdditionEmitter/AdditionEmitterTransform.cs
+++ cs/syncomp/tests/Transformer/Emitters/AdditionEmitter/AdditionEmitterTransform.cs
@@ -1,19 +1,35 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using syncomp;
 
 namespace syncomp.Tests
 {
   [TestClass]
-  public class BreakpointEmitterTransform : Behavior
+  public class AdditionEmitterTranform : Behavior
   {
     private List<string> result;
 
     protected override void Given()
     {
-      
+      this.result = new AdditionEmitter()
+        .Transform(
+          new Addition(new IntegerLiteral("1"), new IntegerLiteral("2")),
+          new Context()
+        );
+    }
+
+    [TestMethod]
+    public void TwoRegisterLoads()
+    {
+      Assert.AreEqual(2, result.Where(line => line.Contains("set")).Count());
+    }
+
+    [TestMethod]
+    public void OneAdd()
+    {
+      Assert.AreEqual(1, result.Where(line => line.Contains("add")).Count());
     }
   }
 }
