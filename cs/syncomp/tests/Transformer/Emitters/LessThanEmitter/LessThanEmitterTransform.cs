035fca69dc00362b60a67c0253b62a638ef2c21f
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-12-05 20:47:35 -0700

    Ok. That's better

---
+++ cs/syncomp/tests/Transformer/Emitters/LessThanEmitter/LessThanEmitterTransform.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using syncomp;
+
+namespace syncomp.Tests
+{
+  [TestClass]
+  public class LessThanEmitterTransform : Behavior
+  {
+    private List<string> result;
+
+    protected override void Given()
+    {
+      
+    }
+  }
+}
