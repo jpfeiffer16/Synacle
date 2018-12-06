d85d5045a56cd1d6edebeb79857696446f06e135
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-12-05 21:08:16 -0700

    More tests

--- cs/syncomp/tests/Transformer/Emitters/DecrEmitter/DecrEmitterTransform.cs
+++ cs/syncomp/tests/Transformer/Emitters/DecrEmitter/DecrEmitterTransform.cs
@@ -13,7 +13,43 @@ namespace syncomp.Tests
 
     protected override void Given()
     {
-      
+      var ctx = new Context();
+      ctx.Variables.Add(new Variable
+      {
+        MemoryAddress = "123",
+        Name = "a"
+      });
+      this.result = new DecrEmitter().Transform(
+        new Decr(new Identifier("a")),
+        ctx
+      );
+    }
+
+    [TestMethod]
+    public void OneSetOnReg1()
+    {
+      Assert.AreEqual(
+        1,
+        this.result.Where(line => line.Contains("set reg1 1")).Count()
+      );
+    }
+    
+    [TestMethod]
+    public void OneCall()
+    {
+      Assert.AreEqual(
+        1,
+        this.result.Where(line => line.Contains("call")).Count()
+      );
+    }
+
+    [TestMethod]
+    public void OneWmem()
+    {
+      Assert.AreEqual(
+        1,
+        this.result.Where(line => line.Contains("wmem")).Count()
+      );
     }
   }
 }
