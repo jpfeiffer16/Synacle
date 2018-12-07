0a29349a12a72fa66e119af9a0fe0540541fb541
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-12-06 21:23:25 -0700

    More tests

--- cs/syncomp/tests/Transformer/Emitters/DivisionEmitter/DivisionEmitterTransform.cs
+++ cs/syncomp/tests/Transformer/Emitters/DivisionEmitter/DivisionEmitterTransform.cs
@@ -13,7 +13,28 @@ namespace syncomp.Tests
 
     protected override void Given()
     {
-      
+      this.result = new DivisionEmitter().Transform(
+        new Division(new IntegerLiteral("3"), new IntegerLiteral("1")),
+        new Context()
+      );
+    }
+
+    [TestMethod]
+    public void TwoSets()
+    {
+      Assert.AreEqual(
+        2,
+        this.result.Where(line => line.Contains("set")).Count()
+      );
+    }
+
+    [TestMethod]
+    public void OneCallToDivide()
+    {
+      Assert.AreEqual(
+        1,
+        this.result.Where(line => line.Contains("call >divide")).Count()
+      );
     }
   }
 }
--- cs/syncomp/tests/Transformer/Emitters/EqualEmitter/EqualEmitterTransform.cs
+++ cs/syncomp/tests/Transformer/Emitters/EqualEmitter/EqualEmitterTransform.cs
@@ -13,7 +13,28 @@ namespace syncomp.Tests
 
     protected override void Given()
     {
-      
+      this.result = new EqualEmitter().Transform(
+        new Equal(new IntegerLiteral("1"), new IntegerLiteral("1")),
+        new Context()
+      );
+    }
+
+    [TestMethod]
+    public void TwoSets()
+    {
+      Assert.AreEqual(
+        2,
+        this.result.Where(line => line.Contains("set")).Count()
+      );
+    }
+
+    [TestMethod]
+    public void OneEq()
+    {
+      Assert.AreEqual(
+        1,
+        this.result.Where(line => line.Contains("eq reg0 reg0 reg1")).Count()
+      );
     }
   }
 }
