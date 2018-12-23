bdf7b8a8b1605cc475e5421aaafeef6b622fd1ba
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-12-22 19:32:25 -0700

    Add stdin support to js vm

--- synvm/vm.js
+++ synvm/vm.js
@@ -10,10 +10,11 @@ let startTime;
 program
   .option('-d, --debug', 'Launch interactive debugger')
   .option('-b, --binary [string]', 'Binary to run. Defaults to challenge.bin')
+  .option('-s, --stdin [bool]', 'Fetch binary from stdin')
   .option('-i, --information [bool]', 'Display information about the program once it exits')
   .parse(process.argv);
 
-if (!program.binary) {
+if (!program.binary && !program.stdin) {
   console.error("Must provide  binary with -b. Use --help to see usage");
 }
 
@@ -31,29 +32,46 @@ const memory = stateExists
   };
 
 if (!stateExists) {
-  let buf = fs.readFileSync(program.binary);  
+  if (program.stdin) {
+    let buff = new Buffer([]);
+    process.stdin.on('data', data => {
+      buff = Buffer.concat([buff, data]);
+    });
+    process.stdin.on('end', () => {
+      if (!buff.length) {
+        console.error("No data provided on stdin!");
+        process.exit(1);
+      }
+      init(buff);
+    });
+  } else {
+    fs.readFile(program.binary, init);  
+  }
+}
+
+function init(buf) {
   for (let i = 0; i < buf.length; i += 2) {
     memory.heap[i ? i / 2 : i] = buf.readUInt16LE(i);
   }
-}
 
-const interpreter = Interpreter(memory);
+  const interpreter = Interpreter(memory);
 
-function step() {
-  interpreter.step();
-  if (!memory.stopped) {
-    setImmediate(step);
-  } else {
-    if (program.information) {
-      console.log(moment.duration(moment().diff(startTime)).as('milliseconds'));
+  function step() {
+    interpreter.step();
+    if (!memory.stopped) {
+      setImmediate(step);
+    } else {
+      if (program.information) {
+        console.log(moment.duration(moment().diff(startTime)).as('milliseconds'));
+      }
     }
   }
-}
 
-if (program.debug) {
-  const Debugger = require('./debugger/debugger');
-  Debugger(memory, interpreter, false);
-} else {
-  startTime = moment();
-  step();
+  if (program.debug) {
+    const Debugger = require('./debugger/debugger');
+    Debugger(memory, interpreter, false);
+  } else {
+    startTime = moment();
+    step();
+  }
 }
