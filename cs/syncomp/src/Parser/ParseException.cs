10c80165121253a196239a2cf46e603d1243204d
jpfeiffer16 <joepfeiffer16@gmail.com>
2019-08-09 20:54:17 -0600

    Change ParseException to accept a message

--- cs/syncomp/src/Parser/ParseException.cs
+++ cs/syncomp/src/Parser/ParseException.cs
@@ -9,7 +9,11 @@ namespace syncomp
     public List<SyntaxToken> Tokens { get; }
     public List<AstNode> Nodes { get; }
 
-    public ParseException(int index, List<SyntaxToken> tokens, List<AstNode> nodes)
+    public ParseException(
+        int index,
+        List<SyntaxToken> tokens,
+        List<AstNode> nodes,
+        string message = null) : base(message)
     {
       this.Index = index;
       this.Tokens = tokens;
