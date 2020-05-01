cf90c18ca3906d7f4c0cc97bebaa1d1924d360da
jpfeiffer16 <joepfeiffer16@gmail.com>
2020-04-30 22:55:05 -0600

    Start decompiler

---
+++ cs/syncomp/Syncomp.Decompiler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Syncomp.Decompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bin = File.ReadAllText(args[0]);
+        }
+    }
+}
