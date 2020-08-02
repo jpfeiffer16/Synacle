4e590b66d4bf73497f290e0d67223e047273ce78
jpfeiffer16 <joepfeiffer16@gmail.com>
2020-08-01 19:18:07 -0600

    Fix StaticWeb build

--- cs/syncomp/Syncomp.StaticWeb/build.sh
+++ cs/syncomp/Syncomp.StaticWeb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dotnet build
-cd bin/Debug/netstandard2.0/
+cd bin/Debug/netstandard2.1/
 mono ../../../../mono-wasm/packager.exe --debug --copy=always --out=./publish --asset=./index.html --asset=./script.js ./Syncomp.StaticWeb.dll ./Newtonsoft.Json.dll
